d8dc153d25fa62d578b634f3133dc2fc3b7dc9ca
Dr Kirwin <pkirwin@bsmschool.org>
2018-01-29 12:15:55 -0600

    custom front

--- action/load_images.js
+++ action/load_images.js
@@ -1,1 +1,5 @@
-document.getElementById('image-list').innerHTML = 'JS!';
+function show_image(event){
+  file_name = event.target.innerHTML;
+  document.getElementById('current-image-title').innerHTML = "<p>"+file_name+"</p>";
+  document.getElementById('current-image').innerHTML = "<img src='/pictures/" + file_name + "'></img>";
+}
--- build_html_file.py
+++ build_html_file.py
@@ -4,7 +4,17 @@ folder = '/home/pi/Pictures/cleanliness/'
 contents = os.listdir(folder)
 
 f = open('/home/pi/cleanliness/action/observe.html', 'w')
+f.write("<link rel='stylesheet' type='text/css' href='format_page.css'>")
+f.write("<div class='image-container'>")
+f.write("<div id='current-image-title'></div>")
+f.write("<div id='current-image'></div>")
+f.write("</div>")
+f.write("<div class='list-container'>")
+i = 0
 for c in list(reversed(sorted(contents))):
-  f.write("<p>"+c+"</p>\n")
-f.write("<script src='action/load_images.js'></script>")
+  i += 1
+  f.write("<p style='cursor:pointer;color:#f30;' class='btn' id='id"+str(i)+"' onclick='show_image(event);'>"+c+"</p>\n")
+f.write("</div>")
+f.write("<script src='load_images.js'></script>")
+f.write("<script src='format_page.css'></script>")
 f.close
